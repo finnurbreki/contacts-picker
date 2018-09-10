2cb3809e32fcdd6a447934fe9fba5e6e79b338b4
Finnur Thorarinsson <finnur@chromium.org>
2018-09-10 11:31:43 +0000

    Show contact image

--- app/src/main/java/com/example/finnur/contactspicker/ContactDetails.java
+++ app/src/main/java/com/example/finnur/contactspicker/ContactDetails.java
@@ -4,6 +4,7 @@
 
 package com.example.finnur.contactspicker;
 
+import android.graphics.Bitmap;
 import android.support.annotation.Nullable;
 import android.util.JsonWriter;
 
@@ -27,19 +28,24 @@ public class ContactDetails implements Comparable<ContactDetails> {
     // The list of phone numbers registered for this contact.
     private List<String> mPhoneNumbers;
 
+    // The image associated with this contact.
+    private Bitmap mBitmap;
+
     /**
      * The ContactDetails constructor.
      * @param id The unique identifier of this contact.
      * @param displayName The display name of this contact.
      * @param emails The emails registered for this contact.
      * @param phoneNumbers The phone numbers registered for this contact.
+     * @param bitmap The image associated with this contact.
      */
     public ContactDetails(
-            String id, String displayName, List<String> emails, List<String> phoneNumbers) {
+            String id, String displayName, List<String> emails, List<String> phoneNumbers, Bitmap bitmap) {
         mDisplayName = displayName;
         mEmails = emails;
         mPhoneNumbers = phoneNumbers;
         mId = id;
+        mBitmap = bitmap;
     }
 
     /**
@@ -48,6 +54,14 @@ public class ContactDetails implements Comparable<ContactDetails> {
      */
     public String getDisplayName() {
         return mDisplayName;
+    }
+
+    /**
+     * Accessor for the contact image.
+     * @return The image as bitmap.
+     */
+    public Bitmap getContactImage() {
+        return mBitmap;
     }
 
     /**
--- app/src/main/java/com/example/finnur/contactspicker/PickerAdapter.java
+++ app/src/main/java/com/example/finnur/contactspicker/PickerAdapter.java
@@ -5,7 +5,11 @@
 package com.example.finnur.contactspicker;
 
 import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.ContactsContract;
 import android.support.v7.widget.RecyclerView.Adapter;
 import android.support.v7.widget.RecyclerView.ViewHolder;
@@ -14,6 +18,8 @@ import android.view.ViewGroup;
 
 //import org.chromium.chrome.R;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
@@ -88,7 +94,7 @@ public class PickerAdapter extends Adapter<ViewHolder> {
                     mContactsCursor.getColumnIndex(ContactsContract.Contacts._ID));
             String name = mContactsCursor.getString(
                     mContactsCursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME_PRIMARY));
-            contacts.add(new ContactDetails(id, name, getEmails(), getPhoneNumbers()));
+            contacts.add(new ContactDetails(id, name, getEmails(), getPhoneNumbers(), getPhoto()));
         } while (mContactsCursor.moveToNext());
 
         return contacts;
@@ -116,7 +122,7 @@ public class PickerAdapter extends Adapter<ViewHolder> {
         }
 
         ((ContactView) holder.itemView)
-                .initialize(new ContactDetails(id, name, getEmails(), getPhoneNumbers()));
+                .initialize(new ContactDetails(id, name, getEmails(), getPhoneNumbers(), getPhoto()));
     }
 
     private ArrayList<String> getEmails() {
@@ -151,6 +157,26 @@ public class PickerAdapter extends Adapter<ViewHolder> {
         }
         phoneNumberCursor.close();
         return phoneNumbers;
+    }
+
+    private Bitmap getPhoto() {
+        String id = mContactsCursor.getString(
+                mContactsCursor.getColumnIndex(ContactsContract.Contacts._ID));
+        Uri contactUri = ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, Long.parseLong(id));
+        Uri photoUri = Uri.withAppendedPath(contactUri, ContactsContract.Contacts.Photo.CONTENT_DIRECTORY);
+        Cursor cursor = mContentResolver.query(photoUri, new String[] {ContactsContract.Contacts.Photo.PHOTO}, null, null, null);
+        if (cursor == null) return null;
+        try {
+            if (cursor.moveToFirst()) {
+                byte[] data = cursor.getBlob(0);
+                if (data != null) {
+                    return BitmapFactory.decodeStream(new ByteArrayInputStream(data));
+                }
+            }
+        } finally {
+            cursor.close();
+        }
+        return null;
     }
 
     @Override
